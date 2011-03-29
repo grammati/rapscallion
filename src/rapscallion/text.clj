fc526cb21a961f64f484ad07ed109389bfb2ddd0
Chris Perkins <chrisperkins99@gmail.com>
2011-03-29 16:37:08 -0400

    started working on text-based templating language

--- src/rapscallion/text.clj
+++ src/rapscallion/text.clj
@@ -1,2 +1,58 @@
-(ns rapscallion.text)
+(ns rapscallion.text
+  (:import (java.io StringReader))
+  (:use [clojure.walk :only [postwalk-replace]])
+  (:use [clojure.string :only [triml]])
+  (:use [clojure.java.io :only [reader]])
+  (:use [rapscallion.core :only [extract-exprs read-all flatseq]])
+  )
+
+(def ^{:dynamic true} *code-line-marker* "%")
+
+(defn compile-meta [line meta]
+  (println "compile-meta: " line)
+  (let [line (triml line)
+        [directive rest] (.split #"\s+" line 2)]
+    (prn "directive" directive)
+    (prn "rest" rest)
+    (case directive
+      "args" (assoc meta :args (read-all rest))
+      (throw (IllegalArgumentException. (str "Unknown directive " directive))))))
+
+(defn compile-template [s]
+  (let [s (if (string? s) (-> s StringReader. reader) s)
+        lines (line-seq s)
+        X *code-line-marker*
+        special-line? (fn [line]
+                        (.startsWith (triml line) (str X X)))
+        code-line? (fn [line]
+                     (and (.startsWith (triml line) X) (not (special-line? line))))
+        text-line? #(not (code-line? %))
+        ]
+    (loop [[line & more :as lines] lines
+           subs {}
+           meta {}
+           parts []]
+      (cond
+        (nil? line)
+          ; we have consumed all the lines - finish compiling.
+          (let [compiled (->> parts
+                              (interpose " ")
+                              (apply str)
+                              read-all
+                              (postwalk-replace subs))]
+            `(fn [{:keys [~@(:args meta)]}] (flatseq ~@compiled)))
+        (special-line? line)
+          ; special directive, such as %%args foo bar
+          (recur more subs (compile-meta (.substring (triml line) 2) meta) parts)
+        (code-line? line)
+          ; code line, such as %(for [foo bar]
+          (recur more subs meta (conj parts (.substring (triml line) 1)))
+        line
+          ; any other line of text - pull out all adjacent text-lines,
+          ; extract substitutions from the text, and store for later
+          (let [[text-lines more] (split-with text-line? lines)
+                chunk (vec (extract-exprs (apply str text-lines)))
+                sym (gensym)
+                subs (assoc subs sym chunk)]
+            (recur more subs meta (conj parts sym)))))))
 
